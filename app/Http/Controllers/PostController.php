7ea15afee66bf8eb38ba0e57911f76c84c959ab1
Goular <zhaojt_cs@126.com>
2017-07-30 12:05:15 +0800

    添加内容

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //列表
+    public function index()
+    {
+        return view('post/index');
+    }
+
+    //详情页面
+    public function show()
+    {
+
+    }
+
+    //创建页面
+    public function create()
+    {
+
+    }
+
+    //创建逻辑
+    public function store()
+    {
+
+    }
+
+    //编译页面
+    public function edit()
+    {
+
+    }
+
+    //编译逻辑
+    public function update()
+    {
+
+    }
+
+    //删除逻辑
+    public function delete()
+    {
+
+    }
+}
