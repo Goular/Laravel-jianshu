fa47fa07554f0871ca0096d14e010e813788b278
Goular <zhaojt_cs@126.com>
2017-07-30 21:48:46 +0800

    添加内容

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,6 @@ Route::get('/', function () {return view('welcome');});
 //文章列表
 Route::get('/posts','\App\Http\Controllers\PostController@index');
 
-//文章详情
-Route::get('/posts/{post}','\App\Http\Controllers\PostController@show')->where(array('post'=>'(?:(?!create).)*'));
 
 //创建文章
 Route::get('/posts/create','\App\Http\Controllers\PostController@create');
@@ -30,7 +28,9 @@ Route::post('/posts','\App\Http\Controllers\PostController@store');
 Route::get('/posts/{post}/edit','\App\Http\Controllers\PostController@edit');
 Route::post('/posts/{post}','\App\Http\Controllers\PostController@update');
 
-
 //删除文章
 Route::get('/posts/delete','\App\Http\Controllers\PostController@delete');
+
+//文章详情
+Route::get('/posts/{post}','\App\Http\Controllers\PostController@show');
 //******************************文章模块******************************
