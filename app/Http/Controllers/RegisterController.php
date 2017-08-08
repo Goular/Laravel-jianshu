8457c7def0658ce0671131b64cb57f634ccf18af
Goular <zhaojt_cs@126.com>
2017-08-08 11:26:51 +0800

    提交页面

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    //注册页面
+    public function index(){
+
+    }
+
+    //注册行为
+    public function register(){
+
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //个人设置页面
+    public function setting(){
+
+    }
+
+    //个人设置行为
+    public function settingStore(){
+
+    }
+}
