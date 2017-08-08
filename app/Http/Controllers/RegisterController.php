2d4cc9f56d0848e3bd6632c2b898662c47f73263
Goular <zhaojt_cs@126.com>
2017-08-08 16:14:21 +0800

    提交页面的内容

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,17 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
     //注册页面
-    public function index(){
+    public function index()
+    {
         return view('register/index');
     }
 
     //注册行为
-    public function register(){
+    public function register()
+    {
+        //验证
+        $this->validate(request(), [
+            'name' => 'required|min:3|unique:users,name',
+            'email' => 'required|unique:users,email|email',
+            'password' => 'required|min:5|max:10|confirmed'
+        ]);
 
+        //逻辑
+        $name = request('name');
+        $email = request('email');
+        $password = bcrypt(request('password'));
+        $user = User::create(compact('name','email','password'));
+
+        //渲染
+        return redirect('/login');
     }
 }
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -33,7 +33,7 @@
 <div class="container">
 
     <form class="form-signin" method="POST" action="/register">
-        <input type="hidden" name="_token" value="MESUY3topeHgvFqsy9EcM916UWQq6khiGHM91wHy">
+        {{csrf_field()}}
         <h2 class="form-signin-heading">请注册</h2>
         <label for="name" class="sr-only">名字</label>
         <input type="text" name="name" id="name" class="form-control" placeholder="名字" required autofocus>
@@ -43,7 +43,7 @@
         <input type="password" name="password" id="inputPassword" class="form-control" placeholder="输入密码" required>
         <label class="sr-only">重复密码</label>
         <input type="password" name="password_confirmation" class="form-control" placeholder="重复输入密码" required>
-
+        @include('layout/errorMsg')
         <button class="btn btn-lg btn-primary btn-block" type="submit">注册</button>
     </form>
 
