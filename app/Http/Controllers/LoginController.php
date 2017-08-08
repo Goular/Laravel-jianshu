4fb5fb88bc9a7bd2ea0091036619b4029920c794
Goular <zhaojt_cs@126.com>
2017-08-08 11:32:41 +0800

    提交登录页面

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
     //登录页面
     public function index(){
-
+        return view('login/index');
     }
 
     //登录行为
