0f8873725cac14a93f9bbfef96ceff16ab327e89
Goular <zhaojt_cs@126.com>
2017-08-11 00:49:44 +0800

    提交个人信息中心

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -18,18 +19,34 @@ class UserController extends Controller
     }
 
     //个人中心显示页面
-    public function show()
+    public function show(User $user)
+    {
+        //这个人信息，包含关注/粉丝/文章数
+        $user = User::withCount(['posts', 'fans', 'stars'])->find($user->id);
+
+        //这个人的文章列表，取创建时间最新的前10条
+        $posts = $user->posts()->orderBy('created_at', 'desc')->take(10)->get();
+
+        //这个人关注的用户，包含关注用户的 关注/粉丝/文章数
+        $stars = $user->stars;
+        $susers = User::whereIn('id', $stars->pluck('star_id'))->withCount(['stars', 'fans', 'posts'])->get();
+
+        //这个人的粉丝用户，包含粉丝用户的 关注/粉丝/文章数
+        $fans = $user->fans;
+        $fusers = User::whereIn('id', $fans->pluck('fan_id'))->withCount(['stars', 'fans', 'posts'])->get();
+
+        return view('user/show', compact('user','posts', 'susers', 'fusers'));
+    }
+
+    //关注用户
+    public function fan()
     {
         return;
     }
 
-    //关注用户
-    public function fan(){
-        return;
-    }
-
     //取消关注
-    public function unfan(){
+    public function unfan()
+    {
         return;
     }
 }
