73ada1c74ea1801f5ee268ffa1d2387ab26d4bb1
Goular <zhaojt_cs@126.com>
2017-08-12 15:53:48 +0800

    提交内容

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\PostTopic;
 use Illuminate\Http\Request;
 use App\Topic;
 
@@ -12,20 +13,24 @@ class TopicController extends Controller
     {
         //带文章数的专题
         $topic = Topic::withCount('postTopics')->find($topic->id);
-
         //专题的文章列表，按照创建时间倒序排列前10个
         $posts = $topic->posts()->orderBy('created_at', 'desc')->take(10)->get();
-
         //属于我的文章，但是为投稿
         $myposts = \App\Post::authorBy(\Auth::id())->topicNotBy($topic->id)->get();
-
         $user = \Auth::user();
-
-        return view('topic/show', compact('user', 'posts', 'myposts'));
+        return view('topic/show', compact('user', 'topic', 'posts', 'myposts'));
     }
 
     public function submit(Topic $topic)
     {
-        return;
+        $this->validate(request(), [
+            'post_ids' => 'required|array'
+        ]);
+        $post_ids = request('post_ids');
+        $topic_id = $topic->id;
+        foreach ($post_ids as $post_id) {
+            PostTopic::firstOrCreate(compact('post_id','topic_id'));
+        }
+        return back();
     }
 }
