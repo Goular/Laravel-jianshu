d094ad092a34ed75d70ce40f799c5f52d8a41451
Goular <zhaojt_cs@126.com>
2017-08-14 10:00:30 +0800

    提交Auth自动转跳登录页面的操作

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -27,7 +27,7 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $exception
+     * @param  \Exception $exception
      * @return void
      */
     public function report(Exception $exception)
@@ -38,8 +38,8 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception $exception
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $exception)
@@ -50,16 +50,25 @@ class Handler extends ExceptionHandler
     /**
      * Convert an authentication exception into an unauthenticated response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Illuminate\Auth\AuthenticationException $exception
      * @return \Illuminate\Http\Response
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
+        //如果请求是要求JSON的话，直接执行下面的内容
         if ($request->expectsJson()) {
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
 
-        return redirect()->guest(route('login'));
+        //如果是网站，我们将根据auth的模块名字(默认为auth,别的名称,例如:auth:admin等)，将根据模块名来进行登录页面的转跳
+        //这个是官方的写法,这个是route()转跳经常报错的: return redirect()->guest(route('login'));
+        $guards = $exception->guards();
+        if (count($guards) == 0) {
+            return redirect()->guest("/login");
+        }
+        foreach ($guards as $guard) {
+            return redirect()->guest("/" . $guard . "/login");
+        }
     }
 }
